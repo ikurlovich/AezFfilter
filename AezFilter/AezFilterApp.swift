bb450edd93443f27aae6b2431ca4bbb845ddeace
Ilia Kurlovich <ikurlovich@icloud.com>
2024-05-06 18:21:57 +0400

    2. Creating a starting point for the project. Empty project body.

---
+++ AezFilter/AezFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AezFilterApp.swift
+//  AezFilter
+//
+//  Created by Ilia Kurlovich on 06.05.24.
+//
+
+import SwiftUI
+
+@main
+struct AezFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
